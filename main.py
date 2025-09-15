0b2c87b1fa6d4d70add2e5b30c31f1facec6c3f6
lissweg0666-max <liss.weg0666@gmail.com>
2025-09-15 18:50:56 +0300

    demo map complete

--- main.py
+++ main.py
@@ -12,14 +12,18 @@ def demo_zip():
     for v1, v2 , v3 in zip(values1, values2, values3):
         print("v1=", v1, "v2=", v2, "v3=", v3, "sum=", v1 + v2 + v3)
 def demo_map():
-    # TODO...
-    # WIP - work in progress...
-    pass
+    list1 = [1, 2, 3, 5]
+    list2 = [5, 3, 2, 0]
+
+    list_of_powers = list(
+        map(pow, list1, list2)
+    )
+    print(list_of_powers)
 
 def main():
     # print("Hello main")
-    demo_zip()
-
+    #demo_zip()
+    demo_map()
 
 if __name__ == "__main__":
     main()
